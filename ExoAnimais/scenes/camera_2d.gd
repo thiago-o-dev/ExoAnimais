03604dc442297581d972a65be0b13cec7bb88583
DESKTOP-SOPQN43\Thiago <syuloboo@gmail.com>
2025-08-07 17:41:22 -0300

    Tabletop and basic camera controls added

---
+++ ExoAnimais/scenes/camera_2d.gd
@@ -0,0 +1,14 @@
+extends Camera2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_key_pressed(KEY_C):
+		zoom = Vector2(2,2)
+	else:
+		zoom = Vector2(1,1)

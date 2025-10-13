387e286fa729e23636764fa3d4f0d5be3e08bcea
MEGANUBIA\giova <syuloboo@gmail.com>
2025-10-12 22:13:05 -0300

    commit 2gio

--- ExoAnimais/src/objects_manager.gd
+++ ExoAnimais/src/objects_manager.gd
@@ -1,7 +1,10 @@
 extends Node2D
 class_name ObjectsManager
 
-@onready var objects_dictionary : Dictionary[ObjectType]
+var objects_dictionary : Dictionary[int, Array[ObjectGroup.ObjectType]]
+
+func add_to_objects(type : ObjectGroup.ObjectType, index_pos : Array[int]):
+	objects_dictionary
 
 func _ready() -> void:
 	pass # Replace with function body.

afe3416500f3120399c02e0f97f7f3b14075ea5d
DESKTOP-SOPQN43\Thiago <syuloboo@gmail.com>
2025-10-13 19:20:18 -0300

    object_manager problematic type hint fixed

--- ExoAnimais/src/objects_manager.gd
+++ ExoAnimais/src/objects_manager.gd
@@ -1,14 +1,14 @@
 extends Node2D
 class_name ObjectsManager
 
-var objects_dictionary : Dictionary[int, Array[ObjectGroup.ObjectType]]
+var objects_dictionary : Array[ObjectGroup.ObjectType]
 
-func add_to_objects(type : ObjectGroup.ObjectType, index_pos : Array[int]):
-	objects_dictionary
+func add_to_objects(type : ObjectGroup.ObjectType, index_positions : Array[int]):
+	for pos in index_positions:
+		objects_dictionary[pos] = type
 
-func _ready() -> void:
-	pass # Replace with function body.
-
+func get_object_in_pos(pos : int) -> int:
+	return objects_dictionary[pos]
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
